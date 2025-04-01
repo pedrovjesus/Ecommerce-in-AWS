8632ea24ad6972813ab9df556e6f01152a3198cd
pedrovjesus <pedroferreira161jesus@gmail.com>
2025-04-01 20:30:56 -0300

    add: implemented get on controller

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -3,7 +3,14 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import { ProductRepository } from "/opt/nodejs/productsLayer";
+import { DynamoDB } from "aws-sdk";
+import { json } from "stream/consumers";
 
+const productDdb = process.env.PRODUCTS_DDB!;
+const ddbClient = new DynamoDB.DocumentClient();
+
+const productRepository = new ProductRepository(ddbClient, productDdb);
 export async function handler(
   event: APIGatewayProxyEvent,
   context: Context
@@ -16,23 +23,31 @@ export async function handler(
   const method = event.httpMethod;
   if (event.resource === "/products") {
     if (method === "GET") {
-      console.log("GET");
+      console.log("GET /products");
+
+      const products = await productRepository.getAllProducts();
 
       return {
         statusCode: 200,
-        body: JSON.stringify({
-          message: "GET Products - OK",
-        }),
+        body: JSON.stringify(products),
       };
     }
   } else if (event.resource === "/products/{id}") {
     const productId = event.pathParameters!.id as string;
-
     console.log(`GET /products/${productId}`);
-    return {
-      statusCode: 200,
-      body: `GET /products/${productId}`,
-    };
+    try {
+      const product = await productRepository.getProductById(productId);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product),
+      };
+    } catch (error) {
+      console.log((<Error>error).message);
+      return {
+        statusCode: 404,
+        body: (<Error>error).message,
+      };
+    }
   }
 
   return {
